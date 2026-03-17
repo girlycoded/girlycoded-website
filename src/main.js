9a0eb684242b9ec1f2782ba63d6afc38055a0617
girlycoded <imjoosan@gmail.com>
2026-03-17 09:57:35 -0600

    Created site layout, added page placeholders

--- src/main.js
+++ src/main.js
@@ -4,24 +4,9 @@ const navbar = document.querySelector(".navbar")
 // const test_component = import.meta.glob("./components/test.html?raw", {"as": "raw", eager: true})
 
 // console.log(test_component)
-import test from "./components/test.html?raw"
+import navbar_component from "./components/navbar.html?raw"
 
-navbar.innerHTML = test
+navbar.innerHTML = navbar_component
 
 
 // navbar.innerHTML = test_component['./components/test.html']
-navbar.innerHTML = marked("# hello world")
-
-fetch("https://nekoweb.org/api/site/info/girlycoded.nekoweb.org")
-	.then(res => {
-		if(!res.ok) {
-			throw new Error("Network response was not OK")
-		}
-		return res.json()
-	})
-	.then(data => {
-		console.log(data)
-	})
-	.catch(err => {
-		console.error("Fetch error:", err);
-	})
