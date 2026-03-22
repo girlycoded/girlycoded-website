e25d7d14297c8d2c083ecc8c3f781428245d0358
girlycoded <imjoosan@gmail.com>
2026-03-22 15:31:04 -0600

    Status supports emojis

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ console.log("hello world");
 
 var img = document.querySelector("#discordpfp");
 var status_label = document.querySelector("#mystatus");
+var username_label = document.querySelector("#myusername")
 
 const status_colors = {
 	online: "green",
@@ -22,11 +23,13 @@ async function init() {
 
 	const result = await response.json();
 
+	username_label.textContent = "@" + result.data.discord_user.username
 	img.src = `https://cdn.discordapp.com/avatars/634169631472353301/${result.data.discord_user.avatar}?size=128`;
 
 	var status_icon = document.querySelector("#status-icon");
 
 	status_icon.style.backgroundColor = status_colors[result.data.discord_status];
+	status_icon.title = `@${result.data.discord_user.username} is ${result.data.discord_status}`
 
 	for (const key in result.data.activities) {
 		if(!Object.prototype.hasOwnProperty.call(result.data.activities, key)) continue;
@@ -34,7 +37,7 @@ async function init() {
 		const element = result.data.activities[key];
 		
 		if(element.name!="Custom Status") continue;
-		status_label.textContent = element.state?`"${element.state}"`:"";
+		status_label.textContent = (element.emoji?element.emoji.name:"") + (element.state?`"${element.state}"`:"");
 	}
 }
 init();
