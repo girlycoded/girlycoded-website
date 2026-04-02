3c8f652fc5f3a767ab8ef1ce2f724d1c9d78cd9e
girlycoded <imjoosan@gmail.com>
2026-04-02 13:16:53 -0600

    Added april blog page to vite rollup

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,8 @@ export default defineConfig({
 		sitemap: resolve(__dirname, 'sitemap.html'),
 		not_found: resolve(__dirname, 'not_found.html'),
 		
-		march_2026_blog: resolve(__dirname, 'blog/03-2026.html')
+		march_2026_blog: resolve(__dirname, 'blog/03-2026.html'),
+		april_2026_blog: resolve(__dirname, 'blog/04-2026.html')
       },
 	  output: {
 		entryFileNames: 'assets/[name].js',
